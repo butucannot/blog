c66a4b95b484af97f7ca21aabf7c3f9ee899c6ab
moneymagoggang <devantorif@gmail.com>
2025-05-13 22:03:42 +0300

    changed view of header and added container wrapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
        <Header />
+       <div className="px-[20%]">
         {children}
+       </div>
       </body>
     </html>
   );
