530b82414aeee77c8cfcaf2f87c978f47b1d7630
moneymagoggang <devantorif@gmail.com>
2025-05-13 21:51:56 +0300

    base struct

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="bg-black">
         {children}
+        </div>
       </body>
     </html>
   );
