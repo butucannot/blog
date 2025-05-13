a892d64bb14f0689d27c574934a199341a982fcd
moneymagoggang <devantorif@gmail.com>
2025-05-13 21:57:44 +0300

    base header wo/ styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/layout/header/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="bg-black">
+       <Header />
         {children}
-        </div>
       </body>
     </html>
   );
