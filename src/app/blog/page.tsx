3d432ec901db729117edf553887fd48b4e07be21
Andriy <andreydemin153@gmail.com>
2025-05-15 13:27:31 +0300

    initial commit2.0

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useRouter } from 'next/navigation'
+
 export default function Blog() {
+  const router = useRouter()
   const posts = [
     {
-      name: "who is it",
+      name: "Who is it",
       img: "/mujik.png",
       created_at: "2025.01.03",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis mattis nunc, sed faucibus nibh feugiat at. Phasellus molestie lectus odio, dignissim tempor libero pellentesque vel. Vivamus a erat vel massa scelerisque tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce cursus diam eget purus pretium, eu tempor lacus finibus. Duis interdum sit amet lorem ac mollis. Aliquam eu faucibus ligula. Phasellus ultricies dictum porta. Vivamus non dapibus nunc. Aenean commodo sapien non tincidunt bibendum.",
@@ -25,7 +29,7 @@ export default function Blog() {
         <h1 className="text-[#FFFFFF] text-[36px] mb-[20px]">BLOG</h1>
         <div className="grid grid-cols-3 gap-20 ">
           {posts.map((obj, index) => (
-              <div key={index} className="bg-[#D9D9D9] h-[300px] rounded-[30px] p-[20px] overflow-hidden hover:bg-[#D9DFD9] cursor-pointer transition duration-300 hover:scale-[1.01]"> 
+              <div key={index} className="bg-[#D9D9D9] h-[300px] rounded-[30px] p-[20px] overflow-hidden hover:bg-[#D9DFD9] cursor-pointer transition duration-300 hover:scale-[1.01]" onClick={() => router.push(`/blog/${obj.name.toLowerCase().replace(/\s/g, '-')}`)}> 
                 <img className="h-[140px] w-[100%] rounded-[30px]" src={obj.img}/>
                 <div className="flex justify-center text-[20px] font-bold ">{obj.name}</div>
                 <div>{obj.content}</div>
