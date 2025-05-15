163dee505f820ce8ef307f798deb2abffc330a36
Andriy <andreydemin153@gmail.com>
2025-05-15 14:35:41 +0300

    ParticularPostPage

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,14 @@
 "use client";
-export default async function Post({params}:any) {
-
-    console.log(params);   
-    const { slug } = await params 
-    const title = slug.replace(/-/g, ' ');
-    console.log(slug);
-    return (
-        <main>
-            <h1 className="text-[#FFFFFF] text-[36px] mb-[20px]">{title}</h1>
-        </main>
-    )
+export default async function Post({ params }: any) {
+  const posts: any = JSON.parse(localStorage.getItem("posts") || "[]");
+//   console.log(params);
+  const { slug } = await params;
+//   console.log(slug);
+  const post = posts.value("name");
+  console.log(post);
+  return (
+    <main>
+      <h1 className="text-[#FFFFFF] text-[36px] mb-[20px]">{slug}</h1>
+    </main>
+  );
 }
